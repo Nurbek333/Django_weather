cab6834a1b0a9ab3cc3183f387828650db6a9e40
rohit56 <rohitacademics01@gmail.com>
2018-09-07 10:52:33 +0530

    Initial Commit modified

---
+++ weather/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class City(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'cities'
+
+
+
---
+++ weather/views.py
@@ -0,0 +1,44 @@
+import requests
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import City
+from .forms import CityForm
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=271d1234d3f497eed5b1d80a07b3fcd1'
+    city = 'London'
+
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+
+    context = {'weather_data': weather_data, 'form': form}
+
+    return render(request, 'weather/weather.html', context)
+
+def delete_everything(self):
+    City.objects.all().delete()
+    return HttpResponseRedirect(reverse('index'))
+
+
+
